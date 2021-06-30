483454a7d15c2567fd84fa08381925b3fda08bb8
kanweiwei <kwwnjujlc@sina.com>
2021-07-01 00:15:30 +0800

    feat: express

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const Vue = require("vue");
+const express = require("express");
+
 const renderder = require("vue-server-renderer").createRenderer();
 
-const app = new Vue({
-  template: `
-        <div id="app">
-            <h1>{{ message }}</h1>
-        </div>
-    `,
-  data: {
-    message: "你好 世界",
-  },
+const server = express();
+
+server.get("/", (req, res) => {
+  const app = new Vue({
+    template: `
+              <div id="app">
+                  <h1>{{ message }}</h1>
+              </div>
+          `,
+    data: {
+      message: "你好 世界",
+    },
+  });
+
+  renderder.renderToString(app, (err, html) => {
+    if (err) {
+        res.status(500).end("Internal server error")
+      return;
+    }
+    res.setHeader("content-type", "text/html;charset=utf8");
+    res.end(html);
+  });
 });
 
-renderder.renderToString(app, (err, html) => {
-  if (err) {
-    return;
-  }
-  console.log(html);
+server.listen(8000, () => {
+  console.log("server running");
 });
