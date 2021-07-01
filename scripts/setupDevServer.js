ad93acda383f80ca6c066b31d6f321739b636031
kanweiwei <kwwnjujlc@sina.com>
2021-07-01 22:47:43 +0800

    开发环境编译

---
+++ scripts/setupDevServer.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const fs = require("fs");
+const chokidar = require("chokidar");
+const webpack = require("webpack");
+const devMiddleware = require("webpack-dev-middleware");
+
+module.exports = (server, cb) => {
+  let ready;
+  const onReady = new Promise((r) => (ready = r));
+
+  // 监视构建 -> 更新 renderer
+  let template;
+  let serverBundle;
+  let clientManifest;
+
+  const update = () => {
+    if (template && serverBundle && clientManifest) {
+      cb({ serverBundle, template, clientManifest });
+      ready();
+    }
+  };
+
+  // 监视 template
+  const templatePath = path.resolve(__dirname, "../index.template.html");
+  template = fs.readFileSync(templatePath, "utf-8");
+  // fs.watch fs.watchFile
+  // chokidar
+  chokidar.watch(templatePath).on("change", () => {
+    template = fs.readFileSync(templatePath, "utf-8");
+    update();
+  });
+
+  // 监视 serverBundle
+  const serverConfig = require("../config/server.webpack.config");
+  const serverCompiler = webpack(serverConfig);
+
+  const instance = devMiddleware(serverCompiler, {
+    logLevel: "silent", // 关闭日志输出
+  });
+
+  serverCompiler.hooks.done.tap("server-build", () => {
+    serverBundle = JSON.parse(
+      instance.fileSystem.readFileSync(
+        path.resolve(__dirname, "../dist/vue-ssr-server-bundle.json"),
+        "utf-8"
+      )
+    );
+    update();
+  });
+
+  // 监视 clientManifest
+  const clientConfig = require("../config/client.webpack.config");
+  const clientCompiler = webpack(clientConfig);
+
+  const clientInstance = devMiddleware(clientCompiler, {
+    publicPath: clientConfig.output.publicPath,
+    logLevel: "silent", // 关闭日志输出
+  });
+
+  // 静态资源在内存中
+  server.use(clientInstance)
+
+  clientCompiler.hooks.done.tap("client-build", () => {
+    clientManifest = JSON.parse(
+      clientInstance.fileSystem.readFileSync(
+        path.resolve(__dirname, "../dist/vue-ssr-client-manifest.json"),
+        "utf-8"
+      )
+    );
+    update();
+  });
+
+  update();
+
+  return onReady;
+};
--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 const Vue = require("vue");
 const express = require("express");
 const fs = require("fs");
+const setupDevServer = require("./scripts/setupDevServer");
 
 const { createBundleRenderer } = require("vue-server-renderer");
-const template = fs.readFileSync("./index.template.html", "utf-8");
 
-const serverBundle = require("./dist/vue-ssr-server-bundle.json");
-const clientManifest = require("./dist/vue-ssr-client-manifest.json");
-
-const renderder = createBundleRenderer(serverBundle, {
-  template,
-  runInNewContext: false,
-  clientManifest,
-});
+const isProd = process.env.NODE_ENV === "production";
 
 const server = express();
 
 server.use("/dist", express.static("dist"));
 
-server.get("/", (req, res) => {
+let renderder;
+let onReady;
+if (isProd) {
+  const template = fs.readFileSync("./index.template.html", "utf-8");
+
+  const serverBundle = require("./dist/vue-ssr-server-bundle.json");
+  const clientManifest = require("./dist/vue-ssr-client-manifest.json");
+
+  renderder = createBundleRenderer(serverBundle, {
+    template,
+    runInNewContext: false,
+    clientManifest,
+  });
+} else {
+  // 开发模式 监视打包
+  onReady = setupDevServer(
+    server,
+    ({ serverBundle, template, clientManifest }) => {
+      renderder = createBundleRenderer(serverBundle, {
+        template,
+        runInNewContext: false,
+        clientManifest,
+      });
+    }
+  );
+}
+
+const render = (req, res) => {
   const context = {
     url: req.url,
     title: "Vue SSR Demo",
@@ -33,7 +53,18 @@ server.get("/", (req, res) => {
     }
     res.end(html);
   });
-});
+};
+
+server.get(
+  "/",
+  isProd
+    ? render
+    : async (req, res) => {
+        // 等待有了 renderer 渲染器后，才调用 render 来渲染
+        await onReady;
+        render(req, res);
+      }
+);
 
 server.listen(8000, () => {
   console.log("server running");
